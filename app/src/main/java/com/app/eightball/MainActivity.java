1a1641d278d05da275f9a250b5aa276eae45f574
Mishael <mishael.harry@cyberspace.net.ng>
2018-02-28 05:06:58 +0100

    completed 8 ball game

--- app/src/main/java/com/app/eightball/MainActivity.java
+++ app/src/main/java/com/app/eightball/MainActivity.java
@@ -2,6 +2,12 @@ package com.app.eightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView ballDisplay = findViewById(R.id.image_eight_ball);
+
+        final int[] ballArray = new int[]{
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+
+        Button myButton = findViewById(R.id.ask_button);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random randomNumber = new Random();
+                int number = randomNumber.nextInt(5);
+                
+                ballDisplay.setImageResource(ballArray[number]);
+            }
+        });
     }
+
+
 }
